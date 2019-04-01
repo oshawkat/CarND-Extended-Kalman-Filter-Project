fbefaabe7d6f93c4934c6b4cbdda91f8c5ecd757
Osman Shawkat <osman.shawkat@havalus.com>
2019-04-01 19:28:41 -0400

    Initialize EKF on first measurement

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -1,7 +1,10 @@
+#define _USE_MATH_DEFINES
+
 #include "FusionEKF.h"
 #include <iostream>
 #include "Eigen/Dense"
 #include "tools.h"
+#include <math.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -18,25 +21,46 @@ FusionEKF::FusionEKF() {
   previous_timestamp_ = 0;
 
   // initializing matrices
+  Eigen::VectorXd x_ = VectorXd(4, 1);
+  Eigen::MatrixXd F_ = MatrixXd(4, 4);
+  Eigen::MatrixXd P_ = MatrixXd(4, 4);
+  Eigen::MatrixXd Q_ = MatrixXd(4, 4);
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
   Hj_ = MatrixXd(3, 4);
 
-  //measurement covariance matrix - laser
+  // Initial state transition matrix
+  F_ << 1, 0, 1, 0,
+        0, 1, 0, 1,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+  // Initial state covariance matrix
+  P_ << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1000, 0,
+        0, 0, 0, 1000;
+
+  // measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
               0, 0.0225;
 
-  //measurement covariance matrix - radar
+  // measurement covariance matrix - radar
   R_radar_ << 0.09, 0, 0,
               0, 0.0009, 0,
               0, 0, 0.09;
+
+  // Laser measurement matrix
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
 
   /**
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
 
+  ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
 
 }
 
@@ -61,13 +85,40 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
+    // Update timestamp
+    previous_timestamp_ = measurement_pack.timestamp_ ;
+
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
 
+      float rho = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float rho_dot = measurement_pack.raw_measurements_[2];
+
+      // Ensure that the phi angle is within [-pi, pi]
+      phi = std::remainder(phi, M_PI);
+
+      // Calculate initial cartesian positions from radial Radar data
+      float px, py;
+      px = rho * cos(phi);
+      py = rho * sin(phi);
+
+      // Radar rho_dot is not a perfect measure of actual velocity but can
+      // still provide some estimate
+      float vx, vy;
+      vx = rho_dot * cos(phi);
+      vy = rho_dot * sin(phi);
+
+      ekf_.x_ << px, py, vx, vy;
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
+      // set the state with the initial location and zero velocity
+      ekf_.x_ << measurement_pack.raw_measurements_[0], 
+                 measurement_pack.raw_measurements_[1], 
+                 0, 0;
 
     }
 
